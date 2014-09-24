f147bf3af7521ce8dbe554a90827c7145b860f15
Ján Zázrivec <jan.zazrivec@gmail.com>
2014-09-24 14:23:54 +0200

    getNotInAppFrames

--- raven/src/main/java/net/kencochrane/raven/DefaultRavenFactory.java
+++ raven/src/main/java/net/kencochrane/raven/DefaultRavenFactory.java
@@ -226,6 +226,7 @@ public class DefaultRavenFactory extends RavenFactory {
                 "org.omg.",
                 "sun.",
                 "junit.",
+                "com.ibm.",
                 "com.intellij.rt.");
     }
 
